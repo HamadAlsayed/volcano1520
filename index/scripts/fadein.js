18063741fcbe96811dc28e305d3a5a2057630761
HamadAlsayed <hamad_sayed@outlook.com>
2024-06-22 21:34:53 +0300

    Add hero video in index.html

---
+++ index/scripts/fadein.js
@@ -0,0 +1,20 @@
+let e = document.getElementById("hero-video");
+e.style.opacity = 0;
+
+let vid = document.getElementById("hero-video");
+vid.oncanplaythrough = function() {
+    setTimeout(function() {
+        let e = document.getElementById('hero-video');
+        fade(e);
+    }, 5000);
+};
+
+function fade(element) {
+    let op = 0;
+    let timer = setInterval(function() {
+        if (op >= 0.3) clearInterval(timer);
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1 || 0.1;
+    }, 100);
+}
